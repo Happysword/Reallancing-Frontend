d25b02a753325c327dff29a620613436177d11d6
Happysword <45149700+Happysword@users.noreply.github.com>
2021-05-02 19:34:12 +0200

    Added admin panel and all its dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <router-view />
 </template>
 
 <script>
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  }),
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,30 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    component: () => import('@/views/Home'),
+  },
+  {
+    path: '/admin',
+    component: () => import('@/views/dashboard/Index'),
+    children: [
+      // Dashboard
+      {
+        name: 'Dashboard',
+        path: '',
+        component: () => import('@/views/dashboard/Dashboard'),
+      },
+      // Pages
+      {
+        name: 'User Profile',
+        path: 'pages/user',
+        component: () => import('@/views/dashboard/pages/UserProfile'),
+      },
+    ],
   },
 ];
 

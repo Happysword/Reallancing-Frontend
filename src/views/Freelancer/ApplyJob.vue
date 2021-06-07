d2e0c5f4d3517a855ad3b000e6b7cd90126b84e1
ahmedx3 <ahmed.moustafaa99@gmail.com>
2021-06-08 00:57:05 +0200

    Made status of proposals

--- src/views/Freelancer/ApplyJob.vue
+++ src/views/Freelancer/ApplyJob.vue
@@ -20,21 +20,42 @@
                 Describe some of your experiences that make you a great candidate for this job.
                 Include any questions you may have about this job, or even request a video call.
               </v-card-title>
-              <v-textarea
-                class="pa-3"
-                outlined
-                name="input-7-4"
-                label="Add cover letter"
-                v-model="coverletter"
-                :rules="[
-                  v => (v || '').length <= 5000 || 'Cover Letter must be less than 5000 characters',
-                  v => (v || '').length >= 50 || 'Cover Letter must be more than 50 characters',
-                ]"
-              ></v-textarea>
+              <v-form ref="firstform">
+                <v-textarea
+                  class="pa-3"
+                  outlined
+                  name="input-7-4"
+                  label="Add cover letter"
+                  v-model="coverletter"
+                  :rules="[
+                    v =>
+                      (v || '').length <= 5000 || 'Cover Letter must be less than 5000 characters',
+                    v => (v || '').length >= 50 || 'Cover Letter must be more than 50 characters',
+                  ]"
+                ></v-textarea>
+                <v-text-field
+                  class="pa-3"
+                  outlined
+                  label="Proposed Hourly Rate"
+                  required
+                  v-model="proposedHourlyRate"
+                  :rules="[rules.required, rules.min]"
+                  dense
+                  type="number"
+                ></v-text-field>
+              </v-form>
             </div>
             <v-divider></v-divider>
             <div class="px-5 py-2">
-              <v-btn rounded color="primary" class="ma-2" @click="createAProposal"
+              <v-btn
+                rounded
+                color="primary"
+                class="ma-2"
+                @click="
+                  () => {
+                    if ($refs.firstform.validate()) createAProposal();
+                  }
+                "
                 >Submit a Proposal</v-btn
               >
             </div>
@@ -52,18 +73,25 @@ export default {
   data() {
     return {
       coverletter: '',
+      proposedHourlyRate: 0,
+      rules: {
+        required: value => !!value || 'Required.',
+        min: v => (!!v && v >= 10 && v <= 10000) || 'Hourly Rate Should be between 10 and 10,000',
+      },
     };
   },
   methods: {
     createAProposal() {
-      api.createAProposal(this.coverletter, this.$route.params.id).then(res => {
-        if (res) {
-          this.$router.push({ name: 'myproposals' });
-          this.$store.state.snackbarMessage = 'Proposal Submitted';
-          this.$store.state.snackbar = true;
-          this.$store.state.snackbarColor = 'primary';
-        }
-      });
+      api
+        .createAProposal(this.coverletter, this.proposedHourlyRate, this.$route.params.id)
+        .then(res => {
+          if (res) {
+            this.$router.push({ name: 'myproposals' });
+            this.$store.state.snackbarMessage = 'Proposal Submitted';
+            this.$store.state.snackbar = true;
+            this.$store.state.snackbarColor = 'primary';
+          }
+        });
     },
   },
 };
